b40284f2cd71c8c1b1147de6e4dff71aff4b3dde
ariel marte <amas_ariel@discente.ufg.br>
2023-01-15 20:29:41 -0300

    Entrega da Atividade

--- light_sensor.py
+++ light_sensor.py
@@ -6,22 +6,27 @@ import time
 __author__ = 'Adapted from Adafruit'
 __license__ = "GPL"
 
+GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)
+redLedPin = 36
+greenLedPin = 37
+GPIO.setup(redLedPin, GPIO.OUT, initial=GPIO.LOW) # Set pin 16 to be an output pin and>
+GPIO.setup(greenLedPin, GPIO.OUT, initial=GPIO.LOW) # Idem for pin 18
 
 #define the pin that goes to the circuit
 pin_to_circuit = 29
 
 def rc_time (pin_to_circuit):
     count = 0
-  
-    #Output on the pin for 
+
+    #Output on the pin for
     GPIO.setup(pin_to_circuit, GPIO.OUT)
     GPIO.output(pin_to_circuit, GPIO.LOW)
     time.sleep(0.1)
 
     #Change the pin back to input
     GPIO.setup(pin_to_circuit, GPIO.IN)
-  
+
     #Count until the pin goes high
     while (GPIO.input(pin_to_circuit) == GPIO.LOW):
         count += 1
@@ -33,11 +38,14 @@ try:
     # Main loop
     while True:
         light_level = rc_time(pin_to_circuit)
-        #print(rc_time(pin_to_circuit))
-        if light_level < 20:
+        if light_level < 400: #Alterado para 400 devido ao capacitor utilizado
             print ('Day')
+            GPIO.output(redLedPin,GPIO.HIGH)
+            GPIO.output(greenLedPin,GPIO.LOW)
         else:
             print ('Night')
+            GPIO.output(redLedPin,GPIO.LOW)
+            GPIO.output(greenLedPin,GPIO.HIGH)
 except KeyboardInterrupt:
     pass
 finally:
